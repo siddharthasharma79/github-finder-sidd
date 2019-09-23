8cf00933cc3fd47253268eb2d15487e4b2630fbc
Siddhartha Sharma <siddharthasharma79@gmail.com>
2019-09-24 01:40:54 +0530

    Complete changes for netlify deployment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
-import Search from './components/users/Search';
-import Users from './components/users/Users';
+import Home from './components/pages/Home';
 import User from './components/users/User';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import './App.css';
 
 import GithubState from './context/github/GithubState';
@@ -22,21 +22,16 @@ const App = () => {
               <Alert />
               <Switch>
                 {/* HOME PAGE */}
-                <Route
-                  exact
-                  path="/"
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
+
                 {/* ABOUT PAGE */}
                 <Route exact path="/about" component={About} />
 
                 {/* USER DETAIL PAGE */}
                 <Route exact path="/user/:login" component={User} />
+
+                {/* NOT FOUND PAGE */}
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
